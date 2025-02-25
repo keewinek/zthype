d484d4ce457b9f12fd7b252aadaea6af85bca888
keewinek <keewinek@gmail.com>
2025-02-25 16:46:06 +0100

    Added try catch to devts

--- dev.ts
+++ dev.ts
@@ -6,10 +6,15 @@ import config from "./fresh.config.ts";
 import "$std/dotenv/load.ts";
 import { start_article_writer_cron } from "./utils/media_mention_article_writer_cron.ts";
 
-const should_start_crons = Deno.env.get("START_CRONS_ON_DEV") === "true";
-
-if (should_start_crons) {
-    start_article_writer_cron();
+try {
+    const should_start_crons = Deno.env.get("START_CRONS_ON_DEV") === "true";
+    
+    if (should_start_crons) {
+        start_article_writer_cron();
+    }
+}
+catch (e) {
+    console.error("Error starting crons:", e);
 }
 
 await dev(import.meta.url, "./main.ts", config);
