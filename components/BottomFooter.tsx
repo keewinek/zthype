77b63410ceda6855737cb747fcddb0a8e35b354d
keewinek <keewinek@gmail.com>
2025-03-02 11:19:47 +0100

    Added bottom footer

---
+++ components/BottomFooter.tsx
@@ -0,0 +1,11 @@
+export default function BottomFooter()
+{
+    return (
+        <>
+            <div class="h-8"></div>
+            <div class="bg-background text-white text-center fixed bottom-0 py-1 w-full z-40">
+                <p>Made with ❤️ by <a href="https://keewinek.netlify.app" target="_blank">keewinek</a>.</p>
+            </div>
+        </>
+    )
+}
