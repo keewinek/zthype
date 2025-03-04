fe5c6b3dd295107bcfd99f7bec4658b8cdf3df98
keewinek <keewinek@gmail.com>
2025-03-04 19:00:14 +0100

    Added test urlid

--- routes/api/get_article.ts
+++ routes/api/get_article.ts
@@ -4,6 +4,7 @@ import * as sdk from "https://deno.land/x/appwrite/mod.ts";
 import { create_order, get_articles_by_queries, get_next_order_id, get_order_by_value } from "../../utils/database.ts";
 import { Article } from "../../interfaces/Article.ts";
 import get_paragraphs_from_article from "../../utils/get_paragraphs_from_article.ts";
+import { Paragraph } from "../../interfaces/Paragraph.ts";
 
 export const handler = async (_req: Request, _ctx: FreshContext): Promise<Response> => {
     const url = new URL(_req.url);
@@ -12,6 +13,12 @@ export const handler = async (_req: Request, _ctx: FreshContext): Promise<Respon
 
     if (!urlid || !source_ids) {
         return new Response(JSON.stringify({"error" : "Missing parameters"}), { status: 400, headers: { "Access-Control-Allow-Origin": "*" } });
+    }
+
+    if (urlid == "test") {
+        // load paragraphs from config/test_paragraphs.json
+        const paragraphs = JSON.parse(await Deno.readTextFile("config/test_paragraphs.json")) as Paragraph[]
+        return new Response(JSON.stringify({"success" : true, "paragraphs" : paragraphs}), { status: 200, headers: { "Access-Control-Allow-Origin": "*" } });
     }
 
     const out_data = await get_articles_by_queries([sdk.Query.equal("urlid", urlid), sdk.Query.equal("source_id", source_ids)]);
